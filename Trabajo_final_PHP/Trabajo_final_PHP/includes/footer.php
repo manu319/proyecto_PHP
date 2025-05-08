7f335cd8b5224be9e9e33e6bdeba3de76faaefed
manu319 <157389966+manu319@users.noreply.github.com>
2025-05-08 15:25:48 +0200

    Delete Trabajo_final_PHP/Trabajo_final_PHP directory

--- Trabajo_final_PHP/Trabajo_final_PHP/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>&copy; 2025 Manu Website. Todos los derechos reservados.</p>
-</footer>
-
-</body>
-</html>
--- Trabajo_final_PHP/Trabajo_final_PHP/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();  // Eliminar todas las variables de sesión
-session_destroy();  // Destruir la sesión
-
-header('Location: index.php');  // Redirigir al inicio
-exit();
-?>
